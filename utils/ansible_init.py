63dae6f36d9bcd81e059c4096dac7a0efa80e133
Bhanu V <vadlamudi.bhanu@gmail.com>
2019-01-29 23:22:36 -0500

    ansibleutils

---
+++ utils/ansible_init.py
@@ -0,0 +1,54 @@
+import yaml
+import os
+
+ANSIBLE_FILE="./ansible/test.yaml"
+SRC_DEST = "/src/testapp/"
+
+def ansible_intialize(config):
+
+	appConfig= config['inputs']['app']
+	#print (appConfig)
+	#add initialize github task
+	git_task = getGitTask(appConfig['git_url'], SRC_DEST)
+	app_task = getAppTask(appConfig['init_file'], SRC_DEST)
+	tasks = ['1',git_task, app_task]
+	print(yaml.safe_dump(tasks, default_flow_style=False))
+
+	with open(ANSIBLE_FILE, 'w') as outfile:
+		yaml.dump(tasks, outfile, default_flow_style=False)
+
+
+def getAppTask(file_name,dest_folder):
+	task={}
+	task['name'] = "run flask app"
+	task['become'] = "True"
+	task['become_method'] = "sudo"
+	task['remote_user'] ="ubuntu"
+	task['shell'] = str("nohup python") + str(dest_folder) + str(file_name)+ str("&")
+	return task
+
+"""
+- 
+    name : "run flask app"
+    become: True
+    become_method: sudo
+    remote_user: ubuntu
+    shell: "nohup python /src/testapp/test.py &"
+"""
+
+
+def getGitTask(repo_url, dest_folder):
+	task= {}
+	task['name'] = "clone git repository"
+	task['git']  = getGit(repo_url, dest_folder)
+	return task
+
+def getGit(repo_url, dest_folder):
+	git = {}
+	git['dest'] 	= dest_folder
+	git['repo'] 	= repo_url
+	git['update']	= "no"
+	git['version']	= "master"
+	git['clone']	= "yes"
+	git['accept_hostkey'] = "yes"
+	return git
